93243f2375d50a760820bd0da40dc99fc19d54fb
c-ggreen <chadggreen@gmail.com>
2026-02-22 15:22:21 -0500

    refactoring, added conversion from csv to list

--- return-and-risk-stock-calculator/src/main.py
+++ return-and-risk-stock-calculator/src/main.py
@@ -1,10 +1,11 @@
-from utils import get_historical_stock_data
+from utils import get_historical_stock_data, csv_to_list
 
 PERIOD = "10y"
 
 
 def main():
     get_historical_stock_data("TSLA", PERIOD)
+    print(csv_to_list())
 
 
 if __name__ == "__main__":
--- return-and-risk-stock-calculator/src/utils.py
+++ return-and-risk-stock-calculator/src/utils.py
@@ -1,11 +1,20 @@
+import csv
 import yfinance as yf
+
+FILENAME = "historical_data.csv"
 
 
 def get_historical_stock_data(ticker: str, period: str) -> None:
     print("Getting historical data...")
     # Get historical data for the stock
     history = yf.Ticker(ticker).history(period=period)
-    history.to_csv("historical_data.csv")
+    history.to_csv(FILENAME)
     print("Data retrieved successfully!")
+
+
+def csv_to_list() -> list[dict[str, str]]:
+    with open(FILENAME, mode="r", newline="", encoding="utf-8") as f:
+        return list(csv.DictReader(f))
+
 
 # TODO: Funcs to calculate: Daily Returns, Cumulative Return, Volatility, Sharpe Ratio
