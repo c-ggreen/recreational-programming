70abec16290d789f69e1ef3027f51ce0afbe29b3
c-ggreen <chadggreen@gmail.com>
2026-02-22 15:54:57 -0500

    refactored stock history file generation

--- return-and-risk-stock-calculator/src/main.py
+++ return-and-risk-stock-calculator/src/main.py
@@ -1,11 +1,19 @@
-from utils import get_historical_stock_data, csv_to_list
+from pathlib import Path
+from utils import load_historical_stock_data, csv_to_list, get_todays_date, FILE_SUFFIX
 
 PERIOD = "10y"
-
+TICKER = "NFLX"
 
 def main():
-    get_historical_stock_data("TSLA", PERIOD)
-    print(csv_to_list())
+    today = get_todays_date()
+    file_path = Path(f"{TICKER}_{today}_{FILE_SUFFIX}")
+
+    # Checking if there is already a file generated today for the stock, 
+    # If not create one, else convert the existing CSV to a list of dicts
+    if not file_path.is_file():
+        load_historical_stock_data(TICKER, PERIOD, today)
+        
+    csv_to_list(TICKER, today)
 
 
 if __name__ == "__main__":
--- return-and-risk-stock-calculator/src/utils.py
+++ return-and-risk-stock-calculator/src/utils.py
@@ -1,20 +1,28 @@
 import csv
+from datetime import datetime as dt
+from zoneinfo import ZoneInfo
+
 import yfinance as yf
 
-FILENAME = "historical_data.csv"
+FILE_SUFFIX = "historical_data.csv"
+tz = ZoneInfo('America/New_York')
 
 
-def get_historical_stock_data(ticker: str, period: str) -> None:
+def load_historical_stock_data(ticker: str, period: str, date: str) -> None:
     print("Getting historical data...")
     # Get historical data for the stock
     history = yf.Ticker(ticker).history(period=period)
-    history.to_csv(FILENAME)
+    history.to_csv(f"{ticker}_{date}_{FILE_SUFFIX}")
     print("Data retrieved successfully!")
 
 
-def csv_to_list() -> list[dict[str, str]]:
-    with open(FILENAME, mode="r", newline="", encoding="utf-8") as f:
+def csv_to_list(ticker: str, date: str) -> list[dict[str, str]]:
+    print("Converting CSV to a list of dictionaries...")
+    with open(f"{ticker}_{date}_{FILE_SUFFIX}", mode="r", newline="", encoding="utf-8") as f:
         return list(csv.DictReader(f))
 
+
+def get_todays_date() -> str:
+    return str(dt.date(dt.now(tz)))
 
 # TODO: Funcs to calculate: Daily Returns, Cumulative Return, Volatility, Sharpe Ratio
