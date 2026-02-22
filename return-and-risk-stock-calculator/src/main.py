71298c6df05c324c391d600f85e35ef35ea7bd49
c-ggreen <chadggreen@gmail.com>
2026-02-22 16:30:53 -0500

    added daily returns feature

--- return-and-risk-stock-calculator/src/main.py
+++ return-and-risk-stock-calculator/src/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-from utils import load_historical_stock_data, csv_to_list, get_todays_date, FILE_SUFFIX
+from utils import load_historical_stock_data, csv_to_list, get_todays_date, FILE_SUFFIX, calculate_daily_returns, daily_returns_to_string
 
-PERIOD = "10y"
+PERIOD = "1y" # y, mo, d, ytd, or max
 TICKER = "NFLX"
 
 def main():
@@ -12,8 +12,10 @@ def main():
     # If not create one, else convert the existing CSV to a list of dicts
     if not file_path.is_file():
         load_historical_stock_data(TICKER, PERIOD, today)
-        
-    csv_to_list(TICKER, today)
+
+    history: list[dict[str, str]] = csv_to_list(TICKER, today)
+
+    daily_returns_to_string(calculate_daily_returns(history))
 
 
 if __name__ == "__main__":
--- return-and-risk-stock-calculator/src/utils.py
+++ return-and-risk-stock-calculator/src/utils.py
@@ -1,11 +1,12 @@
 import csv
 from datetime import datetime as dt
 from zoneinfo import ZoneInfo
+from decimal import Decimal, ROUND_HALF_UP
 
 import yfinance as yf
 
 FILE_SUFFIX = "historical_data.csv"
-tz = ZoneInfo('America/New_York')
+tz = ZoneInfo("America/New_York")
 
 
 def load_historical_stock_data(ticker: str, period: str, date: str) -> None:
@@ -18,11 +19,50 @@ def load_historical_stock_data(ticker: str, period: str, date: str) -> None:
 
 def csv_to_list(ticker: str, date: str) -> list[dict[str, str]]:
     print("Converting CSV to a list of dictionaries...")
-    with open(f"{ticker}_{date}_{FILE_SUFFIX}", mode="r", newline="", encoding="utf-8") as f:
+    with open(
+        f"{ticker}_{date}_{FILE_SUFFIX}", mode="r", newline="", encoding="utf-8"
+    ) as f:
         return list(csv.DictReader(f))
 
 
 def get_todays_date() -> str:
     return str(dt.date(dt.now(tz)))
 
+
 # TODO: Funcs to calculate: Daily Returns, Cumulative Return, Volatility, Sharpe Ratio
+
+
+def calculate_daily_returns(data: list[dict[str, str]]) -> list[list]:
+    output: list[list] = []
+    prev = 0
+    current = 0
+
+    for index, element in enumerate(data):
+        # Skip the first Day but save the Close for the next iteration
+        if index == 0:
+            prev = float(element.get("Close"))
+            continue
+
+        # Getting the date and it's Closing value
+        date: str = element.get("Date").split()[0]
+        current = float(element.get("Close"))
+
+        # Calculating the percentage return
+        percentage_return = ((current / prev) - 1) * 100
+        #Rounding percentage
+        decimal_value = Decimal(str(percentage_return)) 
+        rounded_decimal = decimal_value.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+
+        # Adding the date and return to the output array
+        output.append([date, rounded_decimal])
+
+        # Setting the previous days Close to the current in preparation of the next iteration
+        prev = current
+
+    return output
+
+
+def daily_returns_to_string(data: list[list]) -> None:
+    print("Date\t\tPercentage")
+    for element in data:
+        print(f"{element[0]}\t{element[1]}\r\n")
