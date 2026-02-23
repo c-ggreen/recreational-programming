fcee48228bf22d3f11d2adf8ad8a71291e71ec17
c-ggreen <chadggreen@gmail.com>
2026-02-23 12:04:51 -0500

    reformatting

--- return-and-risk-stock-calculator/src/main.py
+++ return-and-risk-stock-calculator/src/main.py
@@ -1,14 +1,25 @@
 from pathlib import Path
-from utils import load_historical_stock_data, csv_to_list, get_todays_date, FILE_SUFFIX, calculate_daily_returns, daily_returns_to_string, calculate_cumulative_return, calculate_volatility, calculate_sharpe_ratio
+from utils import (
+    load_historical_stock_data,
+    csv_to_list,
+    get_todays_date,
+    FILE_SUFFIX,
+    calculate_daily_returns,
+    daily_returns_to_string,
+    calculate_cumulative_return,
+    calculate_volatility,
+    calculate_sharpe_ratio,
+)
 
-PERIOD = "1y" # y, mo, d, ytd, or max
+PERIOD = "1y"  # y, mo, d, ytd, or max
 TICKER = "AAPL"
+
 
 def main():
     today = get_todays_date()
     file_path = Path(f"{TICKER}_{today}_{FILE_SUFFIX}")
 
-    # Checking if there is already a file generated today for the stock, 
+    # Checking if there is already a file generated today for the stock,
     # If not create one, else convert the existing CSV to a list of dicts
     if not file_path.is_file():
         load_historical_stock_data(TICKER, PERIOD, today)
@@ -25,7 +36,10 @@ def main():
     print(f"Volatility: {volatility:.2f}%")
 
     daily_sharpe, annualized_sharpe = calculate_sharpe_ratio(daily_returns, history)
-    print(f"Daily Sharpe: {daily_sharpe:.2f}, Annualized Sharpe: {annualized_sharpe:.2f}")
+    print(
+        f"Daily Sharpe: {daily_sharpe:.2f}, Annualized Sharpe: {annualized_sharpe:.2f}"
+    )
+
 
 if __name__ == "__main__":
     main()
