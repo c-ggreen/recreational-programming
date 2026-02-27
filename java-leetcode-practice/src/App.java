121a30e1f8eef4e6ccd87f18946da62a2a79711d
c-ggreen <chadggreen@gmail.com>
2026-02-27 18:11:29 -0500

    container with most water

--- java-leetcode-practice/src/App.java
+++ java-leetcode-practice/src/App.java
@@ -1,7 +1,9 @@
+import patterns.TwoPointers.ContainerWithMostWater;
 import patterns.TwoPointers.TwoSum;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(new TwoSum().solution(new int[]{1,3,4,6,8,10,13}, 13));
+        System.out.println(new ContainerWithMostWater().solution(new int[]{3,4,1,2,2,4,1,3,2}));
     }
 }
--- java-leetcode-practice/src/patterns/TwoPointers/ContainerWithMostWater.java
+++ java-leetcode-practice/src/patterns/TwoPointers/ContainerWithMostWater.java
@@ -1,5 +1,27 @@
+// Description
+// Given an array heights where each element represents the height of a vertical line, pick two lines to form a container. Return the maximum area (amount of water) the container can hold.
 package patterns.TwoPointers;
 
 public class ContainerWithMostWater {
-    
+    public int solution(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int max = 0;
+        while (left < right) {
+            int width = right - left;
+            // The height can only be as tall as the lowest wall since the water would spill out of the bucket
+            int height = Math.min(heights[left], heights[right]); 
+            int area = width * height;
+            if (area > max)
+                max = area;
+            
+            // Whichever wall is lower should be moved up or down
+            if (heights[right] > heights[left]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
 }
