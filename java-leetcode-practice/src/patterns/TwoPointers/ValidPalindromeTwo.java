6ca3a70dbe802532868eff449fcc0ea8b2e16160
c-ggreen <chadggreen@gmail.com>
2026-03-02 18:44:16 -0500

    valid palindrom two

---
+++ java-leetcode-practice/src/patterns/TwoPointers/ValidPalindromeTwo.java
@@ -0,0 +1,30 @@
+package patterns.TwoPointers;
+
+public class ValidPalindromeTwo {
+    public boolean solution(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                // Split off into checking if either the skipping the left or right character
+                // will produce a valid palindrome, if not return false
+                return helper(str, i + 1, j) || helper(str, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public boolean helper(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
